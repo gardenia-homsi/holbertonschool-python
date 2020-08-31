33e9b9b178757b2c03487c4ffa5475fa2af5e6ac
Gardenia <60855908+GH780@users.noreply.github.com>
2020-08-31 23:25:33 +0300

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print ("{}".format(str[)object-oriented programming with Python
+str = str[39:65]
 print(str)
