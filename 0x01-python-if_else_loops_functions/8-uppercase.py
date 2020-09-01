a7b925ead5bf42afa783a2344dce384536ea4746
gardinia <gardiniahomsi@gmail.com>
2020-09-01 23:49:27 +0300

    up

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str = ord(str)-32;
+    print("{:c}".format(str))
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last-digit = number % 10;
+    return last-digit;
