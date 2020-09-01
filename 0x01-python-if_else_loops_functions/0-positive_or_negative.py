e4e685c144b27dbb87f310de54c8deda870eaebe
gardinia <gardiniahomsi@gmail.com>
2020-09-01 19:35:10 +0300

    0x01 project

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if number > 0 :
+    print ("{%d} is positive".format (number))
+elif number < 0 :
+    print ("{%d} is negative".format (number))
+else :
+    print ("{%d} is equal to 0".format (number))
+    
+    
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+this is new folder for learning loops and functions 
