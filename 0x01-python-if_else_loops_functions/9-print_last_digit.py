f7a389f0cb49449483754caa8179eafe98d554d2
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-02 22:34:08 +0300

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    digit = number % 10
-    print("{:d}".format(digit))
+    if number < 0:
+        ldigit = -number % 10
+        ldigit = ldigit
+    else:
+        ldigit = number % 10
+    print(ldigit, end="")
+    return (ldigit)
