b02701f6d50547e6d2468a4db4d23529cafd9588
gardinia <gardiniahomsi@gmail.com>
2020-09-02 16:53:24 +0300

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 8):
-    for j in range(1, 9):
+    for j in range(1, 10):
         if (i < j):
             print("{:d}{:d},".format(i, j), end=" ")
-print ("{:d}{:d}".format(i+1, j+1))
+print ("{:d}{:d}".format(i+1, j))
