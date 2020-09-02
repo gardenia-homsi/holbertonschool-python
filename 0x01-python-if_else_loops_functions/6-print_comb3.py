5c2b8c3a2e2fd6d95924b512524d47158c0d7f6c
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-02 16:31:55 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for j in range (1,8):
+    for j in range(1, 8):
         if (i != j):
             print("{:d}{:d},".format(i, j), end=" ")
-print ("{:d}{:d}".format(i+1,j+1))
+print ("{:d}{:d}".format(i+1, j+1))
