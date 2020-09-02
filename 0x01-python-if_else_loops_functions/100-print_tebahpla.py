e6696dcd32be163c567576e650d5597d1cbb8438
gardinia <gardiniahomsi@gmail.com>
2020-09-02 22:20:16 +0300

    advanced

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+str = ""
+for i in reversed(range (97, 123)):
+    if (i % 2) == 0:
+        str += chr(i)
+    else:
+        str += chr(i - 32)
+print("{}".format(str), end="")        
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+def remove_char_at(str,n):
+    s= ""
+    count = 0
+    for c in str:
+        if count == n:
+            pass
+        else:
+            s += c
+            count += 1
+return s
