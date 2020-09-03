13b7721d185c273a461673cba6a077e492f61596
gardinia <gardiniahomsi@gmail.com>
2020-09-03 21:10:41 +0300

    add new file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
         if len(sys.argv) == 1:
                 print("0 arguments.")
         else:
-                print("{:d} arguments:".format(len(sys.argv)))
-                for i in range(2, len(sys.argv)):
+                print("{:d} arguments:".format(len(sys.argv) - 1))
+                for i in range(1, len(sys.argv)):
                         print("{:d} : {}".format(i, sys.argv[i]))
