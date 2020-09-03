7932400eeed5c37205f99cedf88bbef42e4c64d3
gardinia <gardiniahomsi@gmail.com>
2020-09-03 20:23:24 +0300

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) == 1:
+if len(sys.argv) == 0:
         print("0 arguments.")
 else:
-    for i in range (1, len(sys.argv)):
-        print("{:d} : {}".format(i, sys.argv[i]))
+        print("{:d} arguments:".format(len(sys.argv)))
+        for i in range (1, len(sys.argv)):
+              print("{:d} : {}".format(i, sys.argv[i]))
