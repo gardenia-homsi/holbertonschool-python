e833492285350955edd322929b07d9f9f1f6f988
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-03 22:36:07 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
 import re
-for line in hidden_4:
-    for word in line.split():
-        if word != re.findall(r'[__]\w+', word):
-            print(word)
-        
+if __name__ == '__main__':
+    for line in hidden_4:
+        for word in line.split():
+            if word != re.findall(r'[__]\w+', word):
+                print(word)
