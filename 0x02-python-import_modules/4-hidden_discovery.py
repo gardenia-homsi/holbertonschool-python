a3d779c202e2fed6c1bfeeffdc00f1180e00c0e5
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-04 19:33:12 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-import os
-if __name__ == '__main__':
-    for fname in listdir(hidden_4):
-        if fname[0] != '__':
-            print(fname)
+if __name__ == "__main__":
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[0:2] != "__":
+            print(i)
