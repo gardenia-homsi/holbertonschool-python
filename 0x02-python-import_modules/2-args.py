7840bc29275e0c06326ab1b20f3789c269718dd6
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-04 18:39:07 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
+if len(sys.argv) > 1:
+    if __name__ == '__main__':
         print("{:d} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{:d}: {}".format(i, sys.argv[i]))
-    else:
+else:
+    if __name__ == '__main__':
         print("0 arguments.")
