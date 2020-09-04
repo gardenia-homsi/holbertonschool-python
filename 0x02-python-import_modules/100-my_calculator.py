07892a41854310d1148e2660429df9cc74028f87
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-04 18:59:15 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,21 +2,23 @@
 import sys
 from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
+    if len(sys.argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
         if sys.argv[2] == "+":
-            print("{:d} + {:d} = {:d}".format(sys.argv[1], sys.argv[3], add(sys.argv[1], sys.argv[3])))
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
             exit(0)
         elif sys.argv[2] == "-":
-            print("{:d} - {:d} = {:d}".format(sys.argv[1], sys.argv[3], add(sys.argv[1], sys.argv[3])))
+            print("{:d} - {:d} = {:d}".format(a, b, add(a, b)))
             exit(0)
         elif sys.argv[2] == "*":
-            print("{:d} * {:d} = {:d}".format(sys.argv[1], sys.argv[3], add(sys.argv[1], sys.argv[3])))
+            print("{:d} * {:d} = {:d}".format(a, b, add(a, b)))
             exit(0)
         elif sys.argv[2] == "/":
-            print("{:d} / {:d} = {:d}".format(sys.argv[1], sys.argv[3], add(sys.argv[1], sys.argv[3])))
+            print("{:d} / {:d} = {:d}".format(a, b, add(a, b)))
             exit(0)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
