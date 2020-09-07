36b78784f7054a26cc3433a5b6207899d4138ff6
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-07 22:46:41 +0300

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return len(sentence), sentence[0]
+    if len(sentence) == 0:
+        a = None
+    else:
+        a = sentence[0]
+    return len(sentence), a
