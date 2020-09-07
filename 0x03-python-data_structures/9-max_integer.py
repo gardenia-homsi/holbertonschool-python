9ed534179e492dc5456fb03b6856372362f0488d
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-07 22:55:21 +0300

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = 0
+    if leng(my_list) == 0:
+        return None
+    else:
+        for i in range(0, len(my_list)):
+            if max > my_list[i]:
+                max = max
+        else:
+            max = my_list[i]
+    return max
