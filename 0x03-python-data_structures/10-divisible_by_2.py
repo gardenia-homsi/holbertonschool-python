d9349ac4c4a3e2c2189701b23af154820382445e
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-07 23:24:29 +0300

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,14 @@
+  
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return
+
     new_list = []
-    for i in range(0, len(my_list)):
-        if my_list[i] % 2 == 0:
+    for i in my_list:
+        if i % 2 == 0:
             new_list.append(True)
-    else:
-        new_list.append(False)
+        else:
+            new_list.append(False)
+
     return new_list
