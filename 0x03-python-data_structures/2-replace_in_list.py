d843c7452cf44463b78deef4cb3dfb2aaea6b5d2
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-07 20:06:48 +0300

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if (idx < 0) or (idx > len(my_list) - 1):
-        return (my_list[:])
+        return my_list
     else:
         for i in range(0, len(my_list)):
             if (i == idx):
                 my_list[i] = element
-return my_list[:]               
+    return my_list
