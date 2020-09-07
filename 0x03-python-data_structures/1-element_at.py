32b40410ed6e3bc367dc8554c4e55eb5f4afb0cd
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-07 16:28:33 +0300

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         print("None")
     else:
         for i in range(0, len(my_list)):
