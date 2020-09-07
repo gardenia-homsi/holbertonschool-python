8e832b25a3bdaa0c30d7ffbb4d97941eef156188
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-07 22:31:34 +0300

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    na = tuple_a + (0, 0)
+    nb = tuple_b + (0, 0)
+    new_tuple = ((na[0] + nb[0]), (na[1] + nb[1]))
+    return(new_tuple)
