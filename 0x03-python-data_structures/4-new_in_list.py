9a058f8e7c00cc719d32bd4566a8e8f928828f99
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-07 20:40:57 +0300

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list
+    if (idx < 0) or (idx > len(my_list) - 1):
+        return my_list
+    else:
+        for i in range(0, len(my_list)):
+            if (i == idx):
+                new_list[i] = element
+    return new_list
