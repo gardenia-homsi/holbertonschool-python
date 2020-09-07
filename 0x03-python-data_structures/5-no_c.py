d1b6023f1c0183ae4d32fcd1d238b6e341bc012b
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-07 21:25:27 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for i in my_string:
-        if my_string[i] == 'c' or my_string[i] == 'C':
+        if ord(i) != 67 and ord(i) != 99:
             pass
     return my_string
