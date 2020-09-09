01faedc8829ea16476d49920fd2de897719026d1
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-09 21:59:22 +0300

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for k, v in a_dictionary.items():
-        if k == key:
-            a_dictionary[k] = value
+    a_dictionary[key] = value
     return a_dictionary
