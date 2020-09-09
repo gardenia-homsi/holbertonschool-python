4629197143b4badfee4dc82ea4c682e5e0f58b09
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-09 22:25:19 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if len(a_dictionary) == 0:
+        return None
+
+    a = a_dictionary[0]
+    for i in a_dictionary:
+        if a_dictionary[i + 1] > a:
+            a = a_dictionary[i+1]
+
+    return(a)
