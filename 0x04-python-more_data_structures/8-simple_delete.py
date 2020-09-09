7cebdb9a860a9fefbfc0a199d1803808d40df94e
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-09 22:07:12 +0300

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     for k, v in knights.items():
-        if k == (key=""):
+        if k == key:
             del a_dictionary[key]
     return a_dictionary
