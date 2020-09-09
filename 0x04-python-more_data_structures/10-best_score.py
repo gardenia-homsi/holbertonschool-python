7876dd0ccb590aa63033ea3e8303906216b90f37
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-09 22:36:16 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,8 +4,9 @@ def best_score(a_dictionary):
         return None
 
     a = a_dictionary[0]
-    for i in a_dictionary:
-        if a_dictionary[i + 1] > a:
-            a = a_dictionary[i+1]
+    for key in a_dictionary:
+        if a_dictionary[key + 1] > a:
+            a = a_dictionary[key + 1]
+            k = key + 1
 
-    return(a)
+    return(k)
