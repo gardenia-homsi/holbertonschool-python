43200fa177e0fc3b467205768cae2263b851ecd2
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-09 23:31:30 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
-        return None
+    if a_dictionary:
+        max_val = max(a_dictionary.values())
+        for key, value in a_dictionary.items():
+            if max_val == value:
+                return key
 
-    max_val = max(a_dictionary.values())
-    for key, value in a_dictionary.items():
-        if max_val == value:
-            return key
+    return None
