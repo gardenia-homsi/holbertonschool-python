bed121ada88db7b6768ceab890491550ef54f1a1
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-09 23:40:02 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    s = roman_string
-    Rrom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    int_val = 0
-    for i in range(len(s)):
-        if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
-            int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
-        else:
-            int_val += rom_val[s[i]]
-    return int_val
+    if (type(roman_string) != str) or roman_string is None:
+        return 0
+    count = 0
+    tmp = 0
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for iter1 in roman_string:
+        if tmp >= int(dict.get(iter1)) and tmp != 0:
+            count += int(dict.get(iter1))
+        elif tmp < int(dict.get(iter1)) and tmp != 0:
+            count = count + ((int(dict.get(iter1)) - tmp) - tmp)
+        elif tmp == 0:
+            count += int(dict.get(iter1))
+        tmp = int(dict.get(iter1))
+    return (count)
