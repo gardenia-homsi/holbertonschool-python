82e8310b72b4a32c5ae14edc0df7e23ae8b77833
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-10 20:43:40 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,9 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print(my_list[i], end="")
             count += 1
-    except:
+    except IndexError:
         for i in my_list:
             print(my_list[i], end="")
             count += 1
+    print ()
     return count
