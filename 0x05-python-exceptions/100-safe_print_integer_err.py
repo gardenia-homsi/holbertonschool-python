0cd3f9bb8bde5f2f6595f2deaf67543745115ea4
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-10 22:10:36 +0300

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError:
-        Sys.stderr.write("Exception:")
+    except TypeError as err:
+        Sys.stderr.write("Exception:", err)
         return False
