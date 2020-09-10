df34fd220f5201ede47a3a912957ce70c9949396
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-10 20:39:39 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,11 +4,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(0, x):
             print(my_list[i], end="")
-        print(" ")
-        return x
+            count += 1
     except:
         for i in my_list:
             print(my_list[i], end="")
             count += 1
-        print(" ")
-        return count
+    return count
