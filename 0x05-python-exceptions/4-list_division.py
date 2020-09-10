946a06e1002a8723ad03080084ba7b23e916e58f
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-10 20:12:26 +0300

    Update and rename 4-file to 4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = {}
+    for i in range(0, list_length):
+        try:
+            res = my_list_1[i] / muy_list_2[i]
+        except ZeroDivisionError:
+            res = 0
+            print("division by 0")
+        except TypeError:
+            res = 0
+            print("wrong type")
+        except IndexError:
+            res = 0
+            print("out of range")
+        finally:
+            return new_list.append(res)
