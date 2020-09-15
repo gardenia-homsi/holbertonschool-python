2907c349077f0bade94ddec1c90b07dfa9f2382f
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-15 18:09:06 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,16 +7,16 @@ class Square:
     """This class has private size attribute with exceptions."""
     def __init__(self, size=0):
         self.__size = size
-        if type(self.__size) != int:
-            raise TypeError("size must be an integer")
-        if self.__size < 0:
-            raise ValueError("size must be >= 0")
 
     def size(self):
         return self.__size
 
     def size(self, value):
         self.__size = value
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
 
     def area(self):
         """This function return the area of square."""
