d7f9241f949d5ddf72ae740f6f8caab90dd8ca3b
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-15 18:11:50 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,9 +8,11 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
+    @property
     def size(self):
         return self.__size
 
+    @size.setter
     def size(self, value):
         self.__size = value
         if type(self.__size) != int:
