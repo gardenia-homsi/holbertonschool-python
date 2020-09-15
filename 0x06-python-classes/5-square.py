ab02eecea2bf4136528b1b06242d53037ef96e0b
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-15 19:04:04 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -25,7 +25,7 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
-        if self.__size = 0:
+        if self.__size == 0:
             print()
         else:
             for i in range(0, self.__size):
