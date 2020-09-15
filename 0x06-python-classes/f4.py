cb1e26609e0d35a55a6c1c7c7f89285b20cb16a8
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-15 17:59:11 +0300

    Update f4.py

---
+++ 0x06-python-classes/f4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module does one class that has one private integer attribute with 2 exc
+eptions."""
+
+
+class Square:
+    """This class has private size attribute with exceptions."""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        self.__size = value
+
+    def area(self):
+        """This function return the area of square."""
+        return self.__size * self.__size
