12658f4d7c135f7bdcd5785b0fc1a236105d66b8
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-15 16:02:02 +0300

    Update f0.py

---
+++ 0x06-python-classes/f0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/bython3
+'''
+this module is about create an empty class Square
+
+classes:
+
+Square
+
+'''
+
+
+class Square():
+    """
+    a class to represent the Square of numbers
+    """
