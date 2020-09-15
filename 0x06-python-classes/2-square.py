71a173baaeea19f17b0874d2c0a198d22426aba9
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-15 17:19:21 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,10 +4,10 @@ eptions."""
 
 
 class Square:
-    """This class does one private attribute."""
+    """This class has private size attribute with exceptions."""
     def __init__(self, size=0):
-            self.__size = size
-    if type(self.__size) != int:
-        raise TypeError("size must be an integer")
-    if self.__size < 0:
-        raise ValueError("size must be >= 0")
+        self.__size = size
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
