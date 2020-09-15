a2a9122730a92bbb7acbbf2e4b22bcdc4d2e71b1
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-15 16:49:26 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""This module does one class that has one private attribute."""
+
+
 class Square:
+    """This class does one private attribute."""
     __size
     def __init__(self, __size):
         self.__size = __size
