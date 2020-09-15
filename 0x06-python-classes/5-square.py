9e3e0f44276f1e5a8aab1e44958c4d19c2a7032b
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-15 19:01:35 +0300

    Update and rename f5.py to 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This module does one class that has one private integer attribute with 2 exc
+eptions."""
+
+
+class Square:
+    """This class has private size attribute with exceptions."""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """This function return the area of square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size = 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
