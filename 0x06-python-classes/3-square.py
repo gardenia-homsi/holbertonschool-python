06d503b792b4078414ebdc6883ddd5bf8c22e711
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-15 17:32:14 +0300

    Update and rename f3.py to 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module does one class that has one private integer attribute with 2 exc
+eptions."""
+
+
+class Square:
+    """This class has private size attribute with exceptions."""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return self.__size * 4 
