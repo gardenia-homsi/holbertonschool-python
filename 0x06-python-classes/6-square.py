fc82d78b3ff6339de24e2a829d0a4391d0dddbf4
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-15 20:41:00 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,16 +5,20 @@ eptions."""
 
 class Square:
     """This class has private size attribute with exceptions."""
-    def __init__(self, size=0, position=(0, 0)):
-        self.size = size
-        try:
-            self.position = position
-        except TypeError as ti:
-            print(ti)
+    def __init__(self, size=0):
+        self.__size = size
 
     @property
-    def position(self):
-        return self.__position
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
 
     @position.setter
     def position(self, value):
@@ -32,18 +36,6 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        self.__size = value
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
 
     def area(self):
         ''' Returns the area '''
