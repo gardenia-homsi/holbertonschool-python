4cc9a4a08d4f0a418ea6df0411b342a843b5b389
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-17 21:58:12 +0300

    Update 66-square.py

--- 0x06-python-classes/66-square.py
+++ 0x06-python-classes/66-square.py
@@ -6,49 +6,55 @@ eptions."""
 class Square:
     """This class has private size attribute with exceptions."""
     def __init__(self, size=0, position=(0, 0)):
-        self._size = size
-        self._position = position
+        self.size = size
+        try:
+            self.position = position
+        except TypeError as ti:
+            print(ti)
+
+    @property
+    def position(self):
+        """get size value."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """set size value."""
+        if value[0] < 0 or value[1] < 0:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value) != tuple:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) != int or type(value[1]) != int:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     @property
     def size(self):
-        """get size value."""
+        """set size value."""
         return self.__size
 
     @size.setter
     def size(self, value):
         """set size value."""
+        self.__size = value
         if type(value) != int:
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-    @property
-    def position(self):
-        """get position value."""
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        """set position value."""
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(value) != tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(value[0]) != int or type(value[1]) != int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
 
     def area(self):
-        """This function return the area of square."""
+        """return area."""
         return self.__size * self.__size
 
     def my_print(self):
-        """print."""
+        """print the square."""
         if self.__size == 0:
             print()
 
