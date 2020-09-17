15078e1996fe5cb3cb72a93f648041d8f09db7ff
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-17 22:05:08 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,7 +6,11 @@ eptions."""
 class Square:
     """This class has private size attribute with exceptions."""
     def __init__(self, size=0, position=(0, 0)):
-        self.size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
         if len(position) == 2:
             if type(position[0]) != int\
                or type(position[1]) != int or type(position) != tuple:
