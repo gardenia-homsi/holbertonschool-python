7a798ab26076d00f515b4c295d0cfaaa7a5eef4f
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-18 23:24:13 +0300

    Update f4.py

---
+++ 0x07-python-test_driven_developement/f4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module does one class that has one private integer attribute with 2 exc
+eptions."""
+
+
+def print_square(size):
+    """this function is for printing square"""
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if type(size) = int and size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        rasie TypeError("size must be an integer")
+    for i in range(0, size):
+        for j in range(0, size):                
