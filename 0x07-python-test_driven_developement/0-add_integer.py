b2a27cbb7902075347b9f98ce902cd878fec2259
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-18 20:24:15 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_developement/0-add_integer.py
+++ 0x07-python-test_driven_developement/0-add_integer.py
@@ -14,9 +14,9 @@ def add_integer(a, b=98):
       Returns:
       binary_sum (str): Binary string of the sum of a and b
     """
-    if type(a) not string:
+    if type(a) == str:
         raise TypeError"a must be an integer"
-    if type(b) not string:
+    if type(b) == str:
         raise TypeError"b must be an integer"
     if type(a) is float or type(b) is float:
         a = int(a)
