1fd53bc6baf16207fb2fd34524449d2fd3e88990
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-19 19:54:09 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""this module contain one function that add 2 integers and raise exception if not
+"""this module contain one function that add 2 integers
 Functions:def add_integer(a, b=98)
 def add_integer(a) -> integer def add_integer(b=98) -> integer
 return sum of a+b
@@ -10,17 +10,15 @@ def add_integer(a, b=98):
     """Returns the sum of two integer numbers in binary digits.
       Returns:
       binary_sum (str): Binary string of the sum of a and b"""
-    if type(a) == str:
+    if type(a) not int or type(a) is float:
         raise TypeError("a must be an integer")
     if a is None:
         raise TypeError("a must be an integer")
     if b is None:
         raise TypeError("b must be an integer")
-    if type(b) == str:
+    if type(b) != int or type(b) is float:
         raise TypeError("b must be an integer")
     if type(a) is float or type(b) is float:
         a = int(a)
         b = int(b)
-    if a == "" and b == "":
-        raise TypeError("add_integr() missing 1 required positional argument")
     return a+b
