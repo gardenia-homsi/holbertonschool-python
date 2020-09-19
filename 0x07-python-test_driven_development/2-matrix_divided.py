7dbbf011d0df0dfed95a22f8d149550eef4949a7
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-19 21:21:15 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,23 +3,35 @@
 
 
 def matrix_divided(matrix, div):
-    """funct"""
-    i = 0
-    new-matrix = []
+    """function"""
+    try:
+        row_size = len(matrix[0])
+    except IndexError:
+        raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+    cont = 0
+    new_mtx = []
+    result = 0
+
     if type(div) is int or type(div) is float:
         if int(div) == 0:
             raise ZeroDivisionError("division by zero")
     else:
         raise TypeError("div must be a number")
-    for row in matrix:
-        if row-size != len(matrix[i]):
-            rasie TypeError("Each row of the matrix must have the same size")
+
+    for iter1 in matrix:
+        if row_size == len(matrix[cont]):
+            new = []
+            for iter in iter1:
+                if type(iter) is int or type(iter) is float:
+                    result = round(iter / div, 2)
+                    new.append(result)
+#                    iter = iter / div
+                else:
+                    raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+            new_mtx.append(new)
         else:
-            for cell in row:
-                if type(cell) not int or type(cell) not float:
-                    raise TypeError("matrix must be a matrix (list of lists) of 
-                    integers/floats")
-                result = round(cell / div, 2)
-                new-matrix.append(result)
-        i += 1
-    return new-matrix
+            raise TypeError("Each row of the matrix must have the same size")
+        cont += 1
+    return new_mtx
