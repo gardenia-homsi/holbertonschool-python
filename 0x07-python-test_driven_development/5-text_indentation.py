7a6504b13656e9b2f0761fba0d7a64bfc0b32457
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-19 22:03:43 +0300

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,8 +5,11 @@
 def text_indentation(text):
     """Write a function that prints a text with 2 new lines after each of these
     characters: ., ?"""
-    for s in text:
-        if s == "." or s == "?":
-            print()
+    if type(text) is str:
+        for s in text:
+            if s == "." or s == "?":
+                print()
+            else:
+                print(s, end="")
         else:
-            print(s, end="")
+            raise TypeError("text must be a string")
