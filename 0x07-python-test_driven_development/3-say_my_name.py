338c104482523ca9cb3bf9836c96ae32cf5f86a5
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-19 21:39:39 +0300

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,9 +5,10 @@
 def say_my_name(first_name, last_name=""):
     """this function take 2 string parameter and return last name and first nam
     e"""
-    if type(fist_name) not str:
-        raise TypeError("first_name must be a string")
-    elif type(last_name) not str:
-        raise TypeError("last_name must be a string")
+    if type(first_name) is str:
+        if type(last_name) is str:
+            print("My name is {} {}".format(first_name, last_name))
+        else:
+            raise TypeError("last_name must be a string")
     else:
-        print("My name is {} {}".format(fist_name, last_name))
+        raise TypeError("first_name must be a string")
