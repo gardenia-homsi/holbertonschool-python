95a8eaaa9616a098e3c1d2b54e5623b6767b5173
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-19 23:24:57 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""this module contain one function that add 2 integers
-Functions:def add_integer(a, b=98)
-def add_integer(a) -> integer def add_integer(b=98) -> integer
-return sum of a+b
+"""
+    0_add_integer.py
+    This function adds two numbers
+    return a + b
 """
 
 
 def add_integer(a, b=98):
-    """Returns the sum of two integer numbers in binary digits.
-      Returns:
-      binary_sum (str): Binary string of the sum of a and b"""
+
+    """ Function that adds 2 integers
+        return a + b
+    """
+
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int) or isinstance(b, float)):
