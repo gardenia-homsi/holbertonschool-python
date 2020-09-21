1047d7518fa1802edbbaa8a26516257bd9c5a925
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-21 22:47:41 +0300

    Update and rename f1.py to 1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        if type(width) is int:
+            if width > 0:
+                self.__width = width
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+        if type(height) is int:
+            if height > 0:
+                self.__height = height
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+
+
+def width(self):
+    return self._width
+
+
+def width(self, value):
+    if type(value) is int:
+        if value > 0:
+            self.__width = value
+        else:
+            raise ValueError("width must be >= 0")
+    else:
+        raise TypeError("width must be an integer")
+
+
+def height(self):
+    return self.__height
+
+
+def height(self, value):
+    if type(value) is int:
+        if value > 0:
+            self.__height = value
+        else:
+            raise ValueError("height must be >= 0")
+    else:
+        raise TypeError("height must be an integer")
