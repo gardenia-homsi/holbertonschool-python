a304c31fa6cedb8ea71b5d1593c8f79b3271d59a
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-21 22:50:38 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,30 +16,26 @@ class Rectangle:
         else:
             raise TypeError("height must be an integer")
 
+    def width(self):
+        return self._width
 
-def width(self):
-    return self._width
-
-
-def width(self, value):
-    if type(value) is int:
-        if value > 0:
-            self.__width = value
+    def width(self, value):
+        if type(value) is int:
+            if value > 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
         else:
-            raise ValueError("width must be >= 0")
-    else:
-        raise TypeError("width must be an integer")
+            raise TypeError("width must be an integer")
 
+    def height(self):
+        return self.__height
 
-def height(self):
-    return self.__height
-
-
-def height(self, value):
-    if type(value) is int:
-        if value > 0:
-            self.__height = value
+    def height(self, value):
+        if type(value) is int:
+            if value > 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
         else:
-            raise ValueError("height must be >= 0")
-    else:
-        raise TypeError("height must be an integer")
+            raise TypeError("height must be an integer")
