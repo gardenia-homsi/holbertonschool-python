14189026546084362284e6dd1aaef7f6654c2a5f
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-22 23:03:41 +0300

    Update f100.py

---
+++ 0x08-python-more_classes/f100.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""gardenia"""
+
+
+class Rectangle:
+    """function that initiate rectangle"""
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """ Initialice the values """
+        self.width = width
+        self.height = height
+        self.__class__.number_of_instances += 1
+
+    @property
+    def width(self):
+        """getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """return perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """ Redefine the method str to print the rectangle """
+        s = ""
+        if self.width != 0 and self.height != 0:
+            for i in range(self.height):
+                s += str(self.print_symbol) * self.width
+                if i < (self.height - 1):
+                    s += "\n"
+
+        return s
+
+    def __repr__(self):
+        """repr"""
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        self.__class__.number_of_instances -= 1
+
+    def bigger_or_equal(rect_1, rect_2):
+        if isinstance(rect_1, Rectangle):
+            if isinstance(rect_2, Rectangle):
+                if rect_1.area() >= rect_2.area():
+                    return rect_1
+                else:
+                    return rect_2
+            else:
+                raise TypeError("rect_2 must be an instance of Rectangle")
+        else:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+
+    def square(cls, size=0):
+        Rectangle.init(size, size)
