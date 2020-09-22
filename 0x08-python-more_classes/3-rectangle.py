11ec063568673e822ddf8604ea8aace15c034e34
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-22 22:25:23 +0300

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -11,10 +11,12 @@ class Rectangle:
 
     @property
     def width(self):
+        """getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -24,25 +26,18 @@ class Rectangle:
 
     @property
     def height(self):
+        """getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-
-    def area(self):
-        return self.__width * self.__height
-
-    def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        else:
-            return 2 * (self.__width + self.__height)
 
     def area(self):
         return self.__width * self.__height
