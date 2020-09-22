8c71e39ec0ce2103cbc027c221ab20a69f1ce95b
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-22 22:16:38 +0300

    Update 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""gardenia"""
+
+
 class Rectangle:
+    """function rect with area"""
     def __init__(self, width=0, height=0):
         if type(width) is int:
             if width >= 0:
