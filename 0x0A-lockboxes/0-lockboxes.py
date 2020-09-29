57b148ce49ec04157235f7cac7cceb18cd0229bc
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-29 14:26:35 +0300

    Update 0-lockboxes.py

--- 0x0A-lockboxes/0-lockboxes.py
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-    list = [0]
+    list = []
     for i in boxes[0]:
         list.append(i)
     for key in list:
-        list.append(Diff(list, boxes[key])
-        list=RemoveDup(list)
-    if length(list) == lenght(boxes):
+        for j in boxes[key]:
+            list.append(j)
+        list = RemoveDup(list)
+    if length(list) == lenght(boxes) - 1:
         return True
     else:
         return False
 
-                    
-def Diff(li1, li2): 
-    li_dif=[i for i in li1 + li2 if i not in li1 or i not in li2] 
-    return li_dif
 
 def RemoveDup(test_list):
-    res=[] 
-    for i in test_list: 
-        if i not in res: 
-            res.append(i) 
+    res = []
+    for i in test_list:
+        if i not in res:
+            res.append(i)
     return res
