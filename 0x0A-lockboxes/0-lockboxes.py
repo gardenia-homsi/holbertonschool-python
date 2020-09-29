5799901b90d22ffd20783daf28969785c7f5d7d5
Gardenia <60855908+GH780@users.noreply.github.com>
2020-09-29 23:01:42 +0300

    Update 0-lockboxes.py

--- 0x0A-lockboxes/0-lockboxes.py
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-    list = []
-    for i in boxes[0]:
-        list.append(i)
-    for key in list:
-        for j in boxes[key]:
-            list.append(j)
-        list = RemoveDup(list)
-    if length(list) == lenght(boxes) - 1:
+    """ function to check the unblocked boxes inside a list of boxes"""
+    if len(boxes) == None:
+        raise TypeError("boxes can't be NoneType")
+    elif len(boxes) == 1:
         return True
     else:
-        return False
-
-
-def RemoveDup(test_list):
-    res = []
-    for i in test_list:
-        if i not in res:
-            res.append(i)
-    return res
+        list = []
+        if 0 not is list:
+            list.append(0)
+        """loop inside list and append new key boxes for the unchecked one"""
+        for i in list:
+            if i in range(len(boxes)):
+                """loop inside each box of boxes list"""
+                for j in range(len(boxes[i])):
+                    """
+                    add only the new unlocked boxes in the list
+                    """
+                    if boxes[i][j] not in list and boxes[i][j] < len(boxes):
+                        list.append(boxes[i][j])
+        if len(list) == len(boxes):
+            return True
+        else:
+            return False
