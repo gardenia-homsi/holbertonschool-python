1dcf8495cb1b27b99c3463ac6871646751a899b2
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-01 20:26:18 +0300

    Update 2-read_lines.py

--- 0x0C-python-input_output/2-read_lines.py
+++ 0x0C-python-input_output/2-read_lines.py
@@ -1,13 +1,12 @@
-#!/urs/bin/python3
-"""read line"""
+#!/usr/bin/python3
 
 
 def read_lines(filename="", nb_lines=0):
-    """function that read nb of line from a file"""
-    with open(filename, "r") as reader:
-        if nb_lines <= 0 or nb_lines >= len(open(filename).readlines()):
-            for line in reader:
-                print(line, end="")
-        else:
-            for i in range(0, nb_lines):
-                print(reader.readline(), end="")
+    """ Read the file acording to number of required lines """
+    a = 0
+    with open(filename, encoding="utf-8") as reader:
+        for line in reader:
+            print(line, end="")
+            if a == (nb_lines - 1):
+                break
+            a += 1
