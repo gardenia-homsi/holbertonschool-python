0ec075d077af64dbd85094a2b9795bff9e737a74
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-01 23:07:34 +0300

    Update 9-add_item.py

--- 0x0C-python-input_output/9-add_item.py
+++ 0x0C-python-input_output/9-add_item.py
@@ -2,7 +2,7 @@
 """ Load, add, save """
 
 
-import json
+import sys
 import os
 
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
@@ -13,6 +13,6 @@ my_list = []
 if not os.path.exists("./add_item.json"):
     save_to_json_file(list_1, "add_item.json")
 my_list = load_from_json_file("add_item.json")
-for i in range(0, argv):
+for i in range(1, len(argv)):
     my_list.append((argv[i]))
 save_to_json_file(my_list, "add_item.json")
