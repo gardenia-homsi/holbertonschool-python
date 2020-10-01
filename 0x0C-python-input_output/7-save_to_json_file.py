8619ad10c3e43918806a1ea100967d363a3508b8
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-01 22:21:49 +0300

    Update and rename f8.py to 7-save_to_json_file.py

---
+++ 0x0C-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""write object to a file using json representation"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ function"""
+    JsonRep = json.dump(my_obj)
+    with open(filename, 'w') as writer:
+        writer.write(JsonRep)
