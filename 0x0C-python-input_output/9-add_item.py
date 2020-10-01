77286a6618277bda2d53cc63f4390a6cfb784add
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-01 23:08:54 +0300

    Update 9-add_item.py

--- 0x0C-python-input_output/9-add_item.py
+++ 0x0C-python-input_output/9-add_item.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-""" Load, add, save """
+"""
+Python function that add arguments to a Python list
+"""
 import sys
 import os
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+save_json = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
 
-my_list = []
+list_1 = []
+
 if not os.path.exists("./add_item.json"):
-    save_to_json_file(list_1, "add_item.json")
-my_list = load_from_json_file("add_item.json")
-for i in range(1, len(argv)):
-    my_list.append((argv[i]))
-save_to_json_file(my_list, "add_item.json")
+    save_json(list_1, "add_item.json")
+
+list_1 = load_json("add_item.json")
+
+for i in sys.argv[1:]:
+    list_1.append(i)
+
+save_json(list_1, "add_item.json")
