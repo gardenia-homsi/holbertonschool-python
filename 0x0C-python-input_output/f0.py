9a8c302c746b713cefe6953e92808b39407723b9
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-01 19:52:07 +0300

    Update f0.py

--- 0x0C-python-input_output/f0.py
+++ 0x0C-python-input_output/f0.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """function to read file and print output"""
     with open(filename, "r") as reader:
         for line in reader:
             print(line, end="")
