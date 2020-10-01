e1e464292417cfbfcf53ae27b4bfa5fe75cec309
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-01 22:22:28 +0300

    Update 7-save_to_json_file.py

--- 0x0C-python-input_output/7-save_to_json_file.py
+++ 0x0C-python-input_output/7-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ function"""
-    JsonRep = json.dump(my_obj)
+    JsonRep = json.dumps(my_obj)
     with open(filename, 'w') as writer:
         writer.write(JsonRep)
