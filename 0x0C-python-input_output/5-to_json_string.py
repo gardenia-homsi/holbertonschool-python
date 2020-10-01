f06885bfcacee21ab3d3e2a5d854f65b0e86167a
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-01 22:05:53 +0300

    Update and rename f6.py to 5-to_json_string.py

---
+++ 0x0C-python-input_output/5-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" convert to json object"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ returns the JSON representation of an object
+    """
+    return json.dumps(my_obj)
