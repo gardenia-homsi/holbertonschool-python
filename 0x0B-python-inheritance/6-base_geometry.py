0d82b4df2da2ca214d735acb40d7a466fd42bb03
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-03 23:12:56 +0300

    Update and rename f6.py to 6-base_geometry.py

---
+++ 0x0B-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        if self.area = "":
+            raise Exception("area() is not implemented")
+        else:
+            self.area = self
