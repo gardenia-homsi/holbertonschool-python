0dc43de46af437c8c91be95cbe32e36b09cfd460
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-03 23:18:42 +0300

    Update and rename f7.py to 7-base_geometry.py

---
+++ 0x0B-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""contain class BaseGeometry"""
+
+
+class BaseGeometry:
+    """contain 2 functions"""
+    def area(self):
+        """define area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """check value if integer type"""
+        self.name = name
+        self.value = value
+
+        if not isinstance(self.value, int):
+            raise TypeError("{} must be an integer".format(self.name))
+        if self.value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
