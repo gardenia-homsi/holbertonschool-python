452f56374fad130ef916b7e22a3ee9d78d708563
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-03 23:43:56 +0300

    Update and rename f9.py to 9-rectangle.py

---
+++ 0x0B-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""contain Rectangle class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle extends of BaseGeometry """
+
+    def __init__(self, width, height):
+        """define width and height"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return the area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """format string"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
