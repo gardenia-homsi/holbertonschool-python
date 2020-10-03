67cb1eff7961a6c52bf7c31660b75592e4fdaf2c
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-03 23:33:31 +0300

    Update 8-rectangle.py

--- 0x0B-python-inheritance/8-rectangle.py
+++ 0x0B-python-inheritance/8-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle(BaseGeometry):
     """inherete from BaseGeometry"""
     def __init__(self, width, height):
         """define width and height"""
+        super.integer_validator("width", width)
+        super.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        super.integer_validator(width, self.__width)
-        super.integer_validator(width, self.__width)
