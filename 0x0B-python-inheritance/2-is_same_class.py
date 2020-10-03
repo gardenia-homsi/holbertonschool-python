97e95cb5c461bcccb926ca2955d8e7575d87087f
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-03 22:16:07 +0300

    Update 2-is_same_class.py

--- 0x0B-python-inheritance/2-is_same_class.py
+++ 0x0B-python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """function that check class and object"""
-    if isinstance(a_class, object):
+    if isinstance(object, a_class):
         return True
     else:
         return False
