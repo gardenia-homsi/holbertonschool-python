2d0639f002c34d6e598ded9f538fa07e030810fa
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-03 23:52:07 +0300

    Update 10-square.py

--- 0x0B-python-inheritance/10-square.py
+++ 0x0B-python-inheritance/10-square.py
@@ -7,7 +7,12 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """inheret from Rectangle"""
-    def __init__(self, size):
-        """function"""
+     def __init__(self, size):
+        """initiate size"""
         super().__init__(size, size)
         self.__size = size
+        super().integer_validator(self, "size", self.__size)
+
+    def area(self):
+        """Return the area"""
+        return self.__size * self.__size
