308417a679774d1e53ddeb8eb9365eeb611df307
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-04 00:01:04 +0300

    Update 11-square.py

--- 0x0B-python-inheritance/11-square.py
+++ 0x0B-python-inheritance/11-square.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""Module 11-square.py"""
+"""square"""
 
 
-Rectangle = __import__("9-rectangle").Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """This is A Class Called Square"""
-
+    """
+    square
+    """
     def __init__(self, size):
-        """Instance of the class with width and height"""
+        super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-        Rectangle.integer_validator(self, "size", self.__size)
 
     def area(self):
-        """Return the area"""
-        return self.__size * self.__size
+        return super().area()
 
     def __str__(self):
-        """change the print fct output"""
-        return "[{}] {}/{}".format(type(self).__name__,
-                                   self.__size, self.__size)
+        return ("[Square] {}/{}".format(self.__size, self.__size))
