0383359883dead6daf24e296f760f999c863ada0
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-11 19:21:57 +0300

    Update square.py

---
+++ 0x0D-python-almost_a_circle/models/square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""this model contain square class that inheret from rectangle class"""
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(self, size, size, x, y, id)
+
+    def __str__(self):
+        return("[Square] (self.id) self.x/self.y - self.size")
+
+    @property
+    def size(self):
+        """size getter"""
+        retrurn self.size
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        super().width(self, value)
+
+    def update(self, *args, **kwargs):
+        """ It allows don´t have a fixed number of arguments """
+        if args:
+            if len(args) == 4:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            if len(args) == 3:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+            if len(args) == 2:
+                self.id = args[0]
+                self.size = args[1]
+            if len(args) == 1:
+                self.id = args[0]
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, value)
