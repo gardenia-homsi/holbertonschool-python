c0bc33b671b7e493da08256135857f40c7989b4c
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-13 22:40:57 +0300

    Update 0-minoperations.py

--- 0x0E-minimum_operations/0-minoperations.py
+++ 0x0E-minimum_operations/0-minoperations.py
@@ -5,16 +5,13 @@
 def minOperations(n):
     """return min operation"""
     min_operation = 0
+    div = 2
     if n <= 1:
         return 0
-    elif n % 2 == 0 and n % 3 != 0:
-        min_operation = n/2 + 2
-        return min_operation
-    elif n % 3 == 0 and n % 5 != 0:
-        min_operation = int(n/3) + 3
-        return min_operation
-    elif n % 5 == 0:
-        min_operation = n / 5 + 5
-        return min_operation
-    else:
-        return n
+    while div < n:
+        if n % div == 0:
+            min_operation = n / div + div
+            return min_operation
+        else:
+            div += 1
+    return n
