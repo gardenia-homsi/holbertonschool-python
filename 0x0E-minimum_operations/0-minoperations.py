a15b20647f8eef8f57860c71c747c4f0b0560896
gardinia <gardiniahomsi@gmail.com>
2020-10-13 22:06:12 +0300

    complete

---
+++ 0x0E-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""model minoperation"""
+
+
+def minOperations(n):
+    """return min operation"""
+    min_operation = 0
+    if n <=1:
+        return 0
+    elif n % 2 == 0:
+        min_operation = n/2 + 2
+        return min_operation
+    elif n % 3 == 0:
+        min_operation = int(n/3) + 3
+        return min_operation
+    else:
+        return n
+    
