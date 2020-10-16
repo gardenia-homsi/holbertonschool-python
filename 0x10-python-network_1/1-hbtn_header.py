ab97767999e2a41f1438ba0bdbdce49d2ba67f0e
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-16 20:41:19 +0300

    Update 1-hbtn_header.py

--- 0x10-python-network_1/1-hbtn_header.py
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""displays the value of the X-Request-Id variable found in the header of the r
-esponse"""
-
-
-from urllib import (request)
-from sys import argv
+# Python script that displays getheader value
 
 if __name__ == '__main__':
-    with request.urlopen(argv[1]) as header:
+    import urllib.request as ur
+    from sys import argv
+
+    with ur.urlopen(argv[1]) as header:
         print(header.getheader('X-Request-Id'))
