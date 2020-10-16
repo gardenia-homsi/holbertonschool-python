0ce051a06c487166509c7021332862e4ccf40208
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-16 21:53:54 +0300

    Update and rename f5.py to 5-hbtn_header.py

---
+++ 0x10-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""displays the value of the variable X-Request-Id"""
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    print(r.headers.get("X-Request-Id"))
