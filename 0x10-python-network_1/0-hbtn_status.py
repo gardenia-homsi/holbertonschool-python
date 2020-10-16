96a8e0c7bdc53cb939bd366986181f32486d41e9
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-16 15:43:30 +0300

    Update 0-hbtn_status.py

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
 
-print("Body Response:\n\t- type: {}\n\t - content: {}".format(type(html), 
-html))
-print("\t- utf8 content: {}".format(str(type, 'utf-8')))
+    print("Body Response:\n\t- type: {}\n\t - content: {}".format(type(html), 
+    html))
+    print("\t- utf8 content: {}".format(str(type, 'utf-8')))
