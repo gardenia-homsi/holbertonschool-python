b781aaec679663fd26b02c4767a7691ca951b892
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-16 15:28:44 +0300

    Update and rename f0.py to 0-hbtn_status.py

---
+++ 0x10-python_network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this model is to fetch internet resources"""
+
+
+mport urllib.request
+
+if __name__ == "__main__":
+    req = request.Request("https://intranet.hbtn.io/status")
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+print("Body Response:\n\t- type: {}\n\t - content: {}".format(type(html), html))
+print("\t- utf8 content: {}".format(str(type, 'utf-8')))
