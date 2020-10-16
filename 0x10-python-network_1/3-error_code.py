807a39818340f844ff29aa365efc89d83d6a29b3
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-16 21:24:28 +0300

    Update and rename f3.py to 3-error_code.py

---
+++ 0x10-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+manage urllib.error.HTTPError exceptions 
+"""
+
+
+from urllib import (request, parse, error)
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as response:
+            body = response.read()
+
+        print(body.decode('utf8'))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code)
