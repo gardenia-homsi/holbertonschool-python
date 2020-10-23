9a940e703e22bd5056dfa7d1df82f9d48618527e
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-23 22:11:20 +0300

    Update 3-force_name.sql

--- 0x13-SQL_more_queries/3-force_name.sql
+++ 0x13-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
---  creates the table force_name on your MySQL server.
-CREATE TABLE IF NOR EXIST force_name (Id INT, name VARCHAR(256) NOT NULL);
+-- Create database if not exists.
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
