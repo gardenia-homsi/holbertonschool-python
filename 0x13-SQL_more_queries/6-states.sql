e6442f47f3135e6d2d64985a0d2cdeba0f50dcd1
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-23 22:42:33 +0300

    Update 6-states.sql

--- 0x13-SQL_more_queries/6-states.sql
+++ 0x13-SQL_more_queries/6-states.sql
@@ -2,8 +2,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Select database to use.
 USE hbtn_0d_usa;
--- Create table in database with constraint.
-CREATE TABLE IF NOT EXISTES states (
+-- Create user if not exists.
+CREATE TABLE IF NOT EXISTS states(
        id INT UNIQUE AUTO_INCREMENT NOT NULL,
        name VARCHAR(256) NOT NULL,
        PRIMARY KEY(id)
