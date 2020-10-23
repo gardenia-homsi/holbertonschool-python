7c17d146ca89e065144f12762309e527b5d159e5
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-23 20:59:05 +0300

    Update 2-create_read_user.sql

--- 0x13-SQL_more_queries/2-create_read_user.sql
+++ 0x13-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,6 @@
--- creates the database hbtn_0d_2 and the user user_0d_2
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_2_pwd';
+-- Create database if not exists.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create user if not exists.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Privileges of select in a database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
