e65f9e71d7084776900dd63bfa7c869840ca82e6
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-29 22:34:00 +0200

    Create 5-valid_email.sql

---
+++ 0x15-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- creates a trigger that resets the attribute valid_email
+DELIMITER |
+CREATE TRIGGER email_check BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+    SET NEW.valid_email = 0;
+    END IF;
+END;
+|
