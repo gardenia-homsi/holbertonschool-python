8df1a75ca5849771ee0225caf110e806f298dd25
Gardenia <60855908+GH780@users.noreply.github.com>
2020-10-29 22:27:53 +0200

    Update 4-store.sql

--- 0x15-MySQL_Advanced/4-store.sql
+++ 0x15-MySQL_Advanced/4-store.sql
@@ -1,1 +1,5 @@
-
+-- creates a trigger that decreases the quantity
+CREATE TRIGGER order_decrease BEFORE INSERT ON orders
+FOR EACH ROW UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
