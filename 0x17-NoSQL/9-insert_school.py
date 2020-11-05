0d46da4df9ebdcdf8186fb0a681f656b69dea626
Gardenia <60855908+GH780@users.noreply.github.com>
2020-11-05 21:25:14 +0200

    Create 9-insert_school.py

---
+++ 0x17-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+ inserts a new document in a collection based on kwargs
+"""
+
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    insert_school
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
