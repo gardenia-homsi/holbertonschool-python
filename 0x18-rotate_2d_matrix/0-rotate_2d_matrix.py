0dceb27eb31349793da862876e4c2bda401cf024
Gardenia <60855908+GH780@users.noreply.github.com>
2020-11-05 22:04:48 +0200

    Create 0-rotate_2d_matrix.py

---
+++ 0x18-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 0-rotate_2d_matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    rotate 2D matrix
+    """
+    N = len(matrix[0])
+    for i in range(N // 2):
+        for j in range(i, N - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[N - 1 - j][i]
+            matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j]
+            matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i]
+            matrix[j][N - 1 - i] = temp
