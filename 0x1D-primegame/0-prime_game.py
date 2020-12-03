d5991b5673b5030a3194df6558689a089a64c09a
Gardenia <60855908+GH780@users.noreply.github.com>
2020-12-03 23:32:03 +0200

    Update 0-prime_game.py

--- 0x1D-primegame/0-prime_game.py
+++ 0x1D-primegame/0-prime_game.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python3
-""" 0-prime_game.py """
+
+"""
+Module for isWinner.
+"""
 
 
 def isWinner(x, nums):
-    """Solves Prime Game"""
-    if not nums or x < 1:
+    """ Prime numbers game. """
+    if x < 0 or nums[-1] < 1:
         return None
-    n = max(nums)
-    sieve = [True for _ in range(max(n + 1, 2))]
-    for i in range(2, int(pow(n, 0.5)) + 1):
-        if not sieve[i]:
-            continue
-        for j in range(i*i, n + 1, i):
-            sieve[j] = False
+    Maria_score = 0
+    Ben_score = 0
+    prime = [False] * 100001
+    primes = []
+    for i in range(2, 100001):
+        if not prime[i]:
+            primes.append(i)
+            for j in range(i, 100001, i):
+                prime[j] = True
 
-    sieve[0] = sieve[1] = False
-    c = 0
-    for i in range(len(sieve)):
-        if sieve[i]:
-            c += 1
-        sieve[i] = c
-
-    player1 = 0
-    for n in nums:
-        player1 += sieve[n] % 2 == 1
-    if player1 * 2 == len(nums):
-        return None
-    if player1 * 2 > len(nums):
+    l = 0
+    for a0 in range(x):
+        n = nums[l]
+        # your code goes here
+        num = 0
+        for i in range(100001):
+            if i >= len(primes) or primes[i] > n:
+                break
+            num += 1
+        l += 1
+        if (['Maria', 'Ben'][(num % 2) ^ 1]) == "Maria":
+            Maria_score += 1
+        else:
+            Ben_score += 1
+    if Maria_score > Ben_score:
         return "Maria"
-    return "Ben"
+    else:
+        return "Ben"
